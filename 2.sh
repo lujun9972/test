6cb066bb245a807975f485142637ea95a97468bf
darksun <lujun9972@gmail.com>
2018-01-16 12:52:33 +0800

    test

---
+++ 2.sh
@@ -0,0 +1,21 @@
+#!/bin/echo Warinng: this library should be sourced!
+
+mutex_fifo=/tmp/t.fifo
+rm ${mutex_fifo}
+
+function concurrence::mutex_init()
+{
+    mkfifo "${mutex_fifo}"
+    (exec 4<>"${mutex_fifo}";echo >&4)
+}
+
+function concurrence::mutex_lock()
+{
+    (exec 4<>"${mutex_fifo}";read -u 4)
+}
+
+
+concurrence::mutex_init
+echo LOG1
+concurrence::mutex_lock  # 为啥会卡住？？？
+echo LOG2
