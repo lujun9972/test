c3a80d13b40d598bace259ee9246245a919e5d56
darksun <lujun9972@gmail.com>
2018-01-29 19:18:51 +0800

    update

--- concurrence.sh
+++ concurrence.sh
@@ -1,23 +1,24 @@
 #!/bin/echo Warinng: this library should be sourced!
 
-function concurrence::mutex_init()
+function concurrence::Mutex()
 {
     local mutex_fifo=$(mktemp -u "concurrence_mutex_XXXXXX" -p /tmp)
     mkfifo "${mutex_fifo}"
-    (exec 4<>"${mutex_fifo}";echo >&4)
-    echo "${mutex_fifo}"
+    # (exec 4<>"${mutex_fifo}";echo >&4)
+    exec 4<>"${mutex_fifo}";echo >&4
+    eval $1="${mutex_fifo}"
 }
 
 function concurrence::mutex_lock()
 {
     local mutex="$1"
-    (exec 4<>"${mutex}";read -u 4)
+    exec 4<>"${mutex}";read -u 4
 }
 
 function concurrence::mutex_unlock()
 {
     local mutex="$1"
-    (exec 4<>"${mutex}";echo >&4 )
+    exec 4<>"${mutex}";echo >&4 
 }
 
 function concurrence::mutex_destory()
@@ -28,12 +29,12 @@ function concurrence::mutex_destory()
 
 function concurrence::test()
 {
-    mutex1=$(concurrence::mutex_init)
-    echo LOG1 "${mutex1}"
-    concurrence::mutex_lock "${mutex1}" # 为啥会卡住？？？
-    echo LOG2 "${mutex1}"
-    # (concurrence::mutex_lock ${mutex1};sleep 2 ;echo 1;concurrence::mutex_unlock ${mutex1}) &
-    # (concurrence::mutex_lock ${mutex1};echo 2;concurrence::mutex_unlock ${mutex1}) &
+    concurrence::Mutex mutex1
+    # echo LOG1 "${mutex1}"
+    # concurrence::mutex_lock "${mutex1}" # 为啥会卡住？？？
+    # echo LOG2 "${mutex1}"
+    (concurrence::mutex_lock ${mutex1};sleep 2 ;echo 1;concurrence::mutex_unlock ${mutex1}) &
+    (concurrence::mutex_lock ${mutex1};echo 2;concurrence::mutex_unlock ${mutex1}) &
     wait
 }
 
